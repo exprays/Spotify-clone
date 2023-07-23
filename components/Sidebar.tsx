aa5211d4ea8569909cdadf5fdbee53e0530ea834
0rbit0wl <suryakant.subudhi@outlook.com>
2023-07-23 13:40:38 +0530

    🔍Added song search feature to the app

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,13 +7,16 @@ import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
+import { Song } from "@/types";
 
 interface SidebarProps {
     children: React.ReactNode;
+    songs: Song[]
 }
 
 const Sidebar: React.FC<SidebarProps> = ({
-    children
+    children,
+    songs
 }) => {
     const pathname = usePathname();
 
@@ -64,7 +67,7 @@ const Sidebar: React.FC<SidebarProps> = ({
                     
                 </Box>
                 <Box className="overflow-y-auto h-full">
-                    <Library />
+                    <Library songs={songs}/>
                 </Box>
             </div>
             <main className="h-full flex-1 overflow-y-auto py-2">
