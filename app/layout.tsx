b38a2df8d6fb4dc71e7634a42738d95e8a5e9bbc
0rbit0wl <suryakant.subudhi@outlook.com>
2023-07-24 23:17:40 +0530

    🎧Added song player and sound feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
-import getSongsByUserId from '@/actions/getSongsByUserId.ts'
+import getSongsByUserId from '@/actions/getSongsByUserId'
+import Player from '@/components/Player'
 
 const font = Figtree ({ subsets: ['latin'] })
 
@@ -34,6 +35,7 @@ export default async function RootLayout({
                 <Sidebar songs={userSongs}>
                   {children}
                 </Sidebar>
+                <Player />
             </UserProvider>
           </SupabaseProvider>
       </body>
